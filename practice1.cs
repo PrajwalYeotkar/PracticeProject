1b5a3512c796b3b8e166e032507595bee783c225
Prajwal Yeotkar <prajwalyeotkar@gmail.com>
2022-08-16 21:36:47 +0530

    practice1 .cs and .exe file added to display data

---
+++ practice1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+namespace practice1
+{
+    class firstproject
+    {
+        static void Main(string[] args){
+            string filePath = @"C:\Users\prajwal.yeotkar\Desktop\PracticeProject\PracticeProject\studentdata1.txt";
+            List<string> lines = new List<string>();
+            lines  = File.ReadAllLines(filePath).ToList();
+            Console.WriteLine("The Student's data is as follows: ");
+            foreach(string line in lines){
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
